eb4573d00d2c6879cb5ff6a3dc1631add47444cf
Kyle Apsey <kyle@ambrdetroit.com>
2018-07-12 11:03:55 -0400

    front-page styling proto complete

--- front-page.php
+++ front-page.php
@@ -9,9 +9,9 @@ get_header();
 	<div class="container">
 		<div class="hero__inner">
 			<div class="hero__inner-block"></div>
-			<div class="hero__inner-content">
-				<h1 class="heading">TRP. Your new home.</h1>
-				<p class="user-generated">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis metus fringilla, placerat sapien at, maximus odio. Nam ultrices mollis laoreet. Cras quis erat viverra, aliquet purus nec, auctor nisi. Cras vulputate sit amet lorem sed lacinia. Proin ultricies porttitor blandit. Nunc gravida ligula eu leo pulvinar, id imperdiet metus fringilla. Aenean blandit ultrices eros molestie blandit.</p>
+			<div class="hero__inner-content user-generated-content">
+				<h1>TRP. Your new home.</h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer quis metus fringilla, placerat sapien at, maximus odio. Nam ultrices mollis laoreet. Cras quis erat viverra, aliquet purus nec, auctor nisi. Cras vulputate sit amet lorem sed lacinia. Proin ultricies porttitor blandit. Nunc gravida ligula eu leo pulvinar, id imperdiet metus fringilla. Aenean blandit ultrices eros molestie blandit.</p>
 			</div>
 		</div>
 	</div>
@@ -21,8 +21,13 @@ get_header();
 	<div class="pg-home__spotlight">
 		<div class="container">
 			<div class="pg-home__spotlight-inner">
-				<div class="pg-home__spotlight-content"></div>
-				<div class="pg-home__spotlight-gallery"></div>
+				<div class="pg-home__spotlight-block user-generated-content">
+					<h2>Our Mission</h2>
+					<p>Phasellus sagittis felis in eleifend cursus. Duis molestie lorem a urna rhoncus convallis. Sed eu ante odio. Aenean ut molestie metus, eget semper nibh. Ut ex ante, auctor at feugiat a, euismod vel ligula. Donec tristique est eu felis venenatis, eu facilisis turpis interdum. Fusce molestie eget est sed accumsan. Etiam urna orci, gravida a tempor a, cursus ultricies elit. Cras sed tincidunt est. Donec a nulla efficitur, vehicula purus eu, molestie orci. Etiam lobortis, risus eu commodo faucibus, odio nisl vulputate velit, id elementum est ligula sed massa. Cras nec tempor enim.</p>
+				</div>
+				<div class="pg-home__spotlight-block">
+					<img class="pg-home__spotlight-block-image" src="" alt="" />
+				</div>
 			</div>
 		</div>
 	</div>
@@ -30,19 +35,19 @@ get_header();
 		<div class="container">
 			<div class="pg-home__games-inner">
 				<div class="pg-home__games-block">
+					<img class="pg-home__games-block-image" src="" alt=""/>
 					<h2 class="heading heading--alt">Ark: Survival Evolved</h2>
-					<div class="pg-home__games-block-content user-generated">In commodo enim ante, quis blandit massa vehicula quis. Proin mattis mattis metus, sit amet mattis mi. Curabitur eu consequat nibh, quis consectetur felis. Maecenas quis consectetur lorem.</div>
-					<img class="pg-home__games-block-image" src="" alt=""/>
+					<div class="pg-home__games-block-content user-generated-content">In commodo enim ante, quis blandit massa vehicula quis. Proin mattis mattis metus, sit amet mattis mi. Curabitur eu consequat nibh, quis consectetur felis. Maecenas quis consectetur lorem.</div>
 				</div>
 				<div class="pg-home__games-block">
+					<img class="pg-home__games-block-image" src="" alt=""/>
 					<h2 class="heading heading--alt">Grand Theft Auto 5</h2>
-					<div class="pg-home__games-block-content user-generated">In commodo enim ante, quis blandit massa vehicula quis. Proin mattis mattis metus, sit amet mattis mi. Curabitur eu consequat nibh, quis consectetur felis. Maecenas quis consectetur lorem.</div>
-					<img class="pg-home__games-block-image" src="" alt=""/>
+					<div class="pg-home__games-block-content user-generated-content">In commodo enim ante, quis blandit massa vehicula quis. Proin mattis mattis metus, sit amet mattis mi. Curabitur eu consequat nibh, quis consectetur felis. Maecenas quis consectetur lorem.</div>
 				</div>
 				<div class="pg-home__games-block">
-					<h2 class="heading heading--alt">Conan Exiles</h2>
-					<div class="pg-home__games-block-content user-generated">In commodo enim ante, quis blandit massa vehicula quis. Proin mattis mattis metus, sit amet mattis mi. Curabitur eu consequat nibh, quis consectetur felis. Maecenas quis consectetur lorem.</div>
 					<img class="pg-home__games-block-image" src="" alt=""/>
+					<h2 class="heading heading--alt">Conan Exiles</h2>
+					<div class="pg-home__games-block-content user-generated-content">In commodo enim ante, quis blandit massa vehicula quis. Proin mattis mattis metus, sit amet mattis mi. Curabitur eu consequat nibh, quis consectetur felis. Maecenas quis consectetur lorem.</div>
 				</div>
 			</div>
 		</div>
@@ -53,35 +58,35 @@ get_header();
 				<div class="excerpt">
 					<div class="excerpt__image" style="background-image: url('')"></div>
 					<div class="excerpt__details">
-						<h4 class="excerpt__details-title">News Article Title</h4>
+						<h3 class="excerpt__details-title">News Article Title</h3>
 						<div class="excerpt__details-credits">
-							<h5 class="excerpt__details-credits-author">Bob Blogwriter</h5>
+							<h4 class="excerpt__details-credits-author">Bob Blogwriter</h4>
 							<p class="excerpt__details-credits-date">July 12th, 2018</p>
 						</div>
 					</div>
-					<div class="excerpt__content user-generated">Morbi dapibus tortor quis risus venenatis, nec gravida est finibus. Sed ornare fermentum arcu, quis vestibulum tortor volutpat in. Donec turpis massa, bibendum sit amet dui vitae, tempus faucibus tellus. Morbi finibus mollis neque in dignissim. Suspendisse tempor dignissim mauris scelerisque finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque quam nisl, vestibulum ac ipsum id, facilisis consequat lectus. Aenean vestibulum, orci et posuere dapibus, mi augue tincidunt erat, sit amet molestie mauris urna eget nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed laoreet urna id lectus interdum, ut dignissim nunc vulputate. Donec facilisis iaculis tellus eu tempor. Nulla at nulla nec lectus interdum ullamcorper. Vivamus euismod quis magna vitae consequat. Sed elementum vehicula sapien feugiat iaculis. Vivamus consequat sem in massa congue, id rutrum augue porttitor. </div>
+					<div class="excerpt__content user-generated-content">Morbi dapibus tortor quis risus venenatis, nec gravida est finibus. Sed ornare fermentum arcu, quis vestibulum tortor volutpat in. Donec turpis massa, bibendum sit amet dui vitae, tempus faucibus tellus. Morbi finibus mollis neque in dignissim. Suspendisse tempor dignissim mauris scelerisque finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque quam nisl, vestibulum ac ipsum id, facilisis consequat lectus. Aenean vestibulum, orci et posuere dapibus, mi augue tincidunt erat, sit amet molestie mauris urna eget nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed laoreet urna id lectus interdum, ut dignissim nunc vulputate. Donec facilisis iaculis tellus eu tempor. Nulla at nulla nec lectus interdum ullamcorper. Vivamus euismod quis magna vitae consequat. Sed elementum vehicula sapien feugiat iaculis. Vivamus consequat sem in massa congue, id rutrum augue porttitor. </div>
 				</div>
 				<div class="excerpt">
 					<div class="excerpt__image" style="background-image: url('')"></div>
 					<div class="excerpt__details">
-						<h4 class="excerpt__details-title">News Article Title</h4>
+						<h3 class="excerpt__details-title">News Article Title</h3>
 						<div class="excerpt__details-credits">
-							<h5 class="excerpt__details-credits-author">Bob Blogwriter</h5>
+							<h4 class="excerpt__details-credits-author">Bob Blogwriter</h4>
 							<p class="excerpt__details-credits-date">July 12th, 2018</p>
 						</div>
 					</div>
-					<div class="excerpt__content user-generated">Morbi dapibus tortor quis risus venenatis, nec gravida est finibus. Sed ornare fermentum arcu, quis vestibulum tortor volutpat in. Donec turpis massa, bibendum sit amet dui vitae, tempus faucibus tellus. Morbi finibus mollis neque in dignissim. Suspendisse tempor dignissim mauris scelerisque finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque quam nisl, vestibulum ac ipsum id, facilisis consequat lectus. Aenean vestibulum, orci et posuere dapibus, mi augue tincidunt erat, sit amet molestie mauris urna eget nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed laoreet urna id lectus interdum, ut dignissim nunc vulputate. Donec facilisis iaculis tellus eu tempor. Nulla at nulla nec lectus interdum ullamcorper. Vivamus euismod quis magna vitae consequat. Sed elementum vehicula sapien feugiat iaculis. Vivamus consequat sem in massa congue, id rutrum augue porttitor. </div>
+					<div class="excerpt__content user-generated-content">Morbi dapibus tortor quis risus venenatis, nec gravida est finibus. Sed ornare fermentum arcu, quis vestibulum tortor volutpat in. Donec turpis massa, bibendum sit amet dui vitae, tempus faucibus tellus. Morbi finibus mollis neque in dignissim. Suspendisse tempor dignissim mauris scelerisque finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque quam nisl, vestibulum ac ipsum id, facilisis consequat lectus. Aenean vestibulum, orci et posuere dapibus, mi augue tincidunt erat, sit amet molestie mauris urna eget nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed laoreet urna id lectus interdum, ut dignissim nunc vulputate. Donec facilisis iaculis tellus eu tempor. Nulla at nulla nec lectus interdum ullamcorper. Vivamus euismod quis magna vitae consequat. Sed elementum vehicula sapien feugiat iaculis. Vivamus consequat sem in massa congue, id rutrum augue porttitor. </div>
 				</div>
 				<div class="excerpt">
 					<div class="excerpt__image" style="background-image: url('')"></div>
 					<div class="excerpt__details">
-						<h4 class="excerpt__details-title">News Article Title</h4>
+						<h3 class="excerpt__details-title">News Article Title</h3>
 						<div class="excerpt__details-credits">
-							<h5 class="excerpt__details-credits-author">Bob Blogwriter</h5>
+							<h4 class="excerpt__details-credits-author">Bob Blogwriter</h4>
 							<p class="excerpt__details-credits-date">July 12th, 2018</p>
 						</div>
 					</div>
-					<div class="excerpt__content user-generated">Morbi dapibus tortor quis risus venenatis, nec gravida est finibus. Sed ornare fermentum arcu, quis vestibulum tortor volutpat in. Donec turpis massa, bibendum sit amet dui vitae, tempus faucibus tellus. Morbi finibus mollis neque in dignissim. Suspendisse tempor dignissim mauris scelerisque finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque quam nisl, vestibulum ac ipsum id, facilisis consequat lectus. Aenean vestibulum, orci et posuere dapibus, mi augue tincidunt erat, sit amet molestie mauris urna eget nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed laoreet urna id lectus interdum, ut dignissim nunc vulputate. Donec facilisis iaculis tellus eu tempor. Nulla at nulla nec lectus interdum ullamcorper. Vivamus euismod quis magna vitae consequat. Sed elementum vehicula sapien feugiat iaculis. Vivamus consequat sem in massa congue, id rutrum augue porttitor. </div>
+					<div class="excerpt__content user-generated-content">Morbi dapibus tortor quis risus venenatis, nec gravida est finibus. Sed ornare fermentum arcu, quis vestibulum tortor volutpat in. Donec turpis massa, bibendum sit amet dui vitae, tempus faucibus tellus. Morbi finibus mollis neque in dignissim. Suspendisse tempor dignissim mauris scelerisque finibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Pellentesque quam nisl, vestibulum ac ipsum id, facilisis consequat lectus. Aenean vestibulum, orci et posuere dapibus, mi augue tincidunt erat, sit amet molestie mauris urna eget nulla. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed laoreet urna id lectus interdum, ut dignissim nunc vulputate. Donec facilisis iaculis tellus eu tempor. Nulla at nulla nec lectus interdum ullamcorper. Vivamus euismod quis magna vitae consequat. Sed elementum vehicula sapien feugiat iaculis. Vivamus consequat sem in massa congue, id rutrum augue porttitor. </div>
 				</div>
 			</div>
 		</div>
